476264618165e72863b36ecc709910977bfc03e2
李长峰 <109839368+lichangfeng@users.noreply.github.com>
2026-09-26 16:35:28 +0800

    手机版页面改为折叠布局

--- route-planner/service-worker.js
+++ route-planner/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="dance-route-pwa-v5";
+const CACHE="dance-route-pwa-v6";
 const ASSETS=["./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
